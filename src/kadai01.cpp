52f4a9b841f22dc6a082f08016d346ac21845dd5
Ujimoto Yuki <ht19a008@oecu.jp>
2021-11-09 12:14:34 +0900

    is_prime

--- src/kadai01.cpp
+++ src/kadai01.cpp
@@ -1,6 +1,27 @@
 // kadai01.cpp
 #include <iostream>
 #define CPP2_PRIME_UPPER_LIMIT 1000000 //探索する値の上限値。
+
+/* --------------------------------------------------------------- */
+/*
+* is_prime
+*
+* 与えられた正整数 x に対して、素数かどうかを判定する
+*
+*/
+/* -------------------------------------------------------------- */
+bool is_prime(unsigned int x) {
+	int flag = 0;
+	//2からx-1までの数がxを割れるかチェックする。
+	for (int i = 2; i<x; i++) {
+		if (x%i == 0) {
+			return false;	
+		}
+		else {
+			return true;
+		}
+	}
+}
 /* --------------------------------------------------------------- */
 /*
 * nth_prime
@@ -12,9 +33,26 @@
 * ファイルを作成せよ。
 */
 /* -------------------------------------------------------------- */
-int nth_prime(unsigned int a, unsigned int d, unsigned int n);
+int nth_prime(unsigned int a, unsigned int d, unsigned int n) {
+	int num = a;
+	int count = 0;//numが素数であれば1インクリメントする。
+	while (num < 100000) {
+		if (is_prime(num) == true) {
+			count++;
+		}
+		num += d;
+		if (count == n) {
+			return num;
+		}
+	}
+}
+
+
+
+
 int main() {
-	std::cout << nth_prime(367, 186, 151) << std::endl;
+	std::cout << nth_prime(179 ,10 ,203) << std::endl;
+	//std::cout << nth_prime(367, 186, 151) << std::endl;
 	// 以下、同様に、入出力例通りになるか確認せよ。
 	return 0;
 }
