489da5b337039c77c7d9188d2caf36eac589176b
Ujimoto Yuki <ht19a008@oecu.jp>
2022-01-30 18:45:57 +0900

    mcxiプログラム完成

--- ex-2-main.cpp
+++ ex-2-main.cpp
@@ -7,6 +7,8 @@ namespace cpp2 {
 	/*
 	mcxi
 	mcxi 記法を解析するクラスです。
+	「数字が連続で入力されている場合、mcxiの順番が違う場合、mcxiと数字以外の文字が入力されている場合」
+	この3つの不適切な入力が合った場合には例外処理を適用し、またvalue_の値を-1を返すようにしている。
 	*/
 	/* --------------------------------------------------------------------- */
 	class mcxi {
@@ -23,15 +25,32 @@ namespace cpp2 {
 	public:
 		mcxi(const std::string& s) : value_(0) {
 			int num = 0;
+			int ch = 1001;
 			for (auto pos = s.begin(); pos != s.end(); ++pos) {
-				//*pos は、char ! char ってことが分かってたら、もっと簡単にできるのでは？？
 				if (*pos >= '2' && *pos <= '9') {
-					num = (int)(*pos - '0');
-				}
-				else {
+					if (*(pos + 1) >= '0' && *(pos + 1) <= '9') {
+						std::cout << "数字が連続で入力されています" << std::endl;
+						value_ = -1;
+						break;
+					}else {
+						num = (int)(*pos - '0');
+					}
+				}else if(*pos == 'm'|| *pos == 'c' || *pos == 'x' || *pos == 'i'){
 					int u = unit(*pos);
-					value_ += std::max(num, 1) * u;
-					num = 0;
+					if (ch > u) {
+						value_ += std::max(num, 1) * u;
+						ch = u;
+						num = 0;
+					}else {
+						value_ = -1;
+						num = 0;
+						std::cout << "mcxiの順番が違います" << std::endl;
+						break;
+					}
+				}else {
+					value_ = -1;
+					std::cout << "不適切な文字が入力されています" << std::endl;
+					break;
 				}
 			}
 		}
@@ -78,47 +97,37 @@ namespace cpp2 {
 			return ss.str();
 		}
 	};
+	/* --------------------------------------------------------------------- */
+	/*
+	check
+	与えられた回答と計算結果を比較し実装が正しいことを確認する。
+	*/
+	/* --------------------------------------------------------------------- */
+	void check(std::string a, std::string b, std::string answer) {
+		cpp2::mcxi mcxi_a(a);
+		cpp2::mcxi mcxi_b(b);
+		cpp2::mcxi result = mcxi_a + mcxi_b;
+		if (result.to_string() == answer) {
+			std::cout << a << "+" << b << "は与えられた回答" << answer << "と等しいため正解" << std::endl;
+		}else {
+			std::cout << a << "+" << b << "は与えられた回答と異なるため不正解" << std::endl;
+		}
+	}
 } // namespace cpp2
 int main() {
-	cpp2::mcxi a0("xi");
-	cpp2::mcxi b0("x9i");
-	cpp2::mcxi result0 = a0 + b0;
-	std::cout << "3x" << " " << result0.to_string() << std::endl;
-	cpp2::mcxi a1("i");
-	cpp2::mcxi b1("9i");
-	cpp2::mcxi result1 = a1 + b1;
-	std::cout << "x" << " " << result1.to_string() << std::endl;
-	cpp2::mcxi a2("c2x2i");
-	cpp2::mcxi b2("4c8x8i");
-	cpp2::mcxi result2 = a2 + b2;
-	std::cout << "6cx" << " " << result2.to_string() << std::endl;
-	cpp2::mcxi a3("m2ci");
-	cpp2::mcxi b3("4m7c9x8i");
-	cpp2::mcxi result3 = a3 + b3;
-	std::cout << "5m9c9x9i" << " " << result3.to_string() << std::endl;
-	cpp2::mcxi a4("9c9x9i");
-	cpp2::mcxi b4("i");
-	cpp2::mcxi result4 = a4 + b4;
-	std::cout << "m" << " " << result4.to_string() << std::endl;
-	cpp2::mcxi a5("i");
-	cpp2::mcxi b5("9m9c9x8i");
-	cpp2::mcxi result5 = a5 + b5;
-	std::cout << "9m9c9x9i" << " " << result5.to_string() << std::endl;
-	cpp2::mcxi a6("m");
-	cpp2::mcxi b6("i");
-	cpp2::mcxi result6 = a6 + b6;
-	std::cout << "mi" << " " << result6.to_string() << std::endl;
-	cpp2::mcxi a7("i");
-	cpp2::mcxi b7("m");
-	cpp2::mcxi result7 = a7 + b7;
-	std::cout << "mi" << " " << result7.to_string() << std::endl;
-	cpp2::mcxi a8("m9i");
-	cpp2::mcxi b8("i");
-	cpp2::mcxi result8 = a8 + b8;
-	std::cout << "mx" << " " << result8.to_string() << std::endl;
-	cpp2::mcxi a9("9m8c7xi");
-	cpp2::mcxi b9("c2x8i");
-	cpp2::mcxi result9 = a9 + b9;
-	std::cout << "9m9c9x9i" << " " << result9.to_string() << std::endl;
+	cpp2::mcxi c0("ix");//発展課題1のテスト用
+	cpp2::mcxi c1("aa");//発展課題1のテスト用
+	cpp2::mcxi c2("m2x33i");//発展課題1のテスト用
+	cpp2::check("i", "9i", "2x");//発展課題2のテスト用
+	cpp2::check("xi", "x9i", "3x");
+	cpp2::check("i", "9i", "x");
+	cpp2::check("c2x2i", "4c8x8i", "6cx");
+	cpp2::check("m2ci", "4m7c9x8i", "5m9c9x9i");
+	cpp2::check("9c9x9i", "i", "m");
+	cpp2::check("i", "9m9c9x8i", "9m9c9x9i");
+	cpp2::check("m", "i", "mi");
+	cpp2::check("i", "m", "mi");
+	cpp2::check("m9i", "i", "mx");
+	cpp2::check("9m8c7xi", "c2x8i", "9m9c9x9i");
 }
 
